0697bdfc5c58cf4e1b530a47db30e8264ff8cd2c
Knox Hutchinson <knox@knoxsdata.com>
2020-11-10 03:27:54 +0000

    nighttime commit

--- collector.py
+++ collector.py
@@ -11,8 +11,6 @@ from nornir.core.task import Task, Result
 import logging
 from nornir_scrapli.functions import print_structured_result
 
-nr = InitNornir(config_file="config.yaml")
-
 
 # def config_checker():
 #     config_gen()
@@ -21,7 +19,23 @@ nr = InitNornir(config_file="config.yaml")
 def get_inv_details(task):
     response = task.run(task=send_command,
                         command='show interfaces', severity_level=logging.DEBUG)
-    results = response.scrapli_response.textfsm_parse_output()
+    int_results = response.scrapli_response.textfsm_parse_output()
+    ver_response = task.run(
+        task=send_command, command='show version', severity_level=logging.DEBUG)
+    ver_results = ver_response.scrapli_response.textfsm_parse_output()
+    results = {}
+    results['hostname'] = ver_results[0]['hostname']
+    results['platform'] = ver_results[0]['rommon']
+    results['serial'] = ver_results[0]['serial'][0]
+    results['interfaces'] = []
+    for int in int_results:
+        interface = {}
+        interface['name'] = int['interface']
+        interface['mac'] = int['bia']
+        interface['ip'] = int['ip_address']
+        interface['media'] = int['media_type']
+        interface['active'] = int['link_status']
+        results['interfaces'].append(interface)
     return Result(host=task.host, result=results)
 
 
@@ -29,5 +43,6 @@ if __name__ == "__main__":
     response = input("Do you want to gen a config first? (y/N): ")
     if response == "y":
         config_gen()
+    nr = InitNornir(config_file="config.yaml")
     results = nr.run(task=get_inv_details)
     print_result(results)
---
+++ knetbox.py
@@ -0,0 +1,57 @@
+import pynetbox
+from collector import get_inv_details
+from configer import config_gen
+from nornir import InitNornir
+from nornir_scrapli.tasks import (
+    get_prompt,
+    send_command,
+    send_commands,
+    send_configs
+)
+from nornir_utils.plugins.functions import print_result
+from nornir.core.task import Task, Result
+import logging
+from nornir_scrapli.functions import print_structured_result
+
+
+nb = pynetbox.api(
+    'http://10.10.21.196:8000',
+    token='0123456789abcdef0123456789abcdef01234567')
+
+print(nb.dcim.sites.all())
+print(nb.dcim.device_roles.all())
+print(nb.dcim.manufacturers.all())
+print(nb.dcim.device_types.all())
+
+
+def device_creator(task):
+    device = task.run(task=get_inv_details)
+    hostname = device['hostname']
+    print(nb.dcim.sites.all())
+    site = input("Choose a site from above list: ")
+    role = 'Virtual'
+    for int in device['interfaces']:
+        if int['media'] != 'Virtual':
+            role = int['media']
+    if device['platform'] in ('IOS-XE', 'IOS', 'NX-OS'):
+        manu = 'Cisco'
+    print(nb.dcim.device_types.all())
+    dev_type = input("Select a device type from above: ")
+    try:
+        results = nb.dcim.devices.create(name=hostname,
+                                         device_role=role,
+                                         device_type=dev_type,
+                                         site=site,
+                                         status="Active"
+                                         )
+    except Exception as err:
+        results = err
+    return Result(host=task.host, result=results)
+
+
+device_creator()
+
+# if __name__ == "__main__":
+#     nr = InitNornir(config_file="config.yaml")
+#     results = nr.run(task=device_creator)
+#     print_result(results)
