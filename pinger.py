ae4a6d22dedcc801ae48166df5697b0c5e2080d6
Knox Hutchinson <knox@knoxsdata.com>
2020-11-11 02:10:46 +0000

    update pinger

--- pinger.py
+++ pinger.py
@@ -1,21 +1,44 @@
 import subprocess
 import ipaddress
 from subprocess import Popen, PIPE
+import platform
+import argparse
 
 
 def test_hosts():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--subnet', type=str,
+                        dest="subnet", help='Provide topology name.')
+    args = parser.parse_args()
+    if args.subnet:
+        subnet = args.subnet
+    else:
+        subnet = input("Please enter the network: ")
+
     alive_hosts = []
-    subnet = input("Please enter the network: ")
+
+    current_os = platform.system().lower()
+    print(current_os)
+
     network = ipaddress.ip_network(subnet)
+
+    if current_os == "windows":
+        parameter = "-n"
+    else:
+        parameter = "-c"
     for i in network.hosts():
         i = str(i)
-        toping = subprocess.Popen(['ping', '-c', '1', i], stdout=PIPE)
+        toping = subprocess.Popen(['ping', parameter, '5', i], stdout=PIPE)
         output = toping.communicate()[0]
         hostalive = toping.returncode
         if hostalive == 0:
-            print(i, 'is reachable')
+            print(i, ': !!!!!')
             alive_hosts.append(i)
 
         else:
-            print(i, 'is down')
+            print(i, ': .....')
     return alive_hosts
+
+
+if __name__ == "__main__":
+    test_hosts()
